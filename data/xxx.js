1c24b1ddcef62e09dcb6e83c9edb080bf8df88d4
Jonathan Turkanis <j@coderage.com>
2023-01-05 03:19:05 +0000

    Misc

--- data/xxx.js
+++ data/xxx.js
@@ -10,8 +10,24 @@ data = {
       text: "Creekside Community Master Association aka Creeksi c/o Robert E. Weiss, Incorporated 920 South Village Oaks Drive Covina, CA 91724",
       annotations: [
         {
+          class: "accept",
+          label: "Accepted",
+          entities : [
+            [1, 'EntityProper', [[0, 9]]],
+            [2, 'EntityDescriptive', [[10, 38]]],
+            [3, 'Alias', [[39, 42]]],
+            [4, 'EntityProper', [[43, 50]]],
+            [5, 'CareOf', [[51, 54]]],
+            [7, 'FirstName', [[55, 61]]],
+            [8, 'MiddleName', [[62, 64]]],
+            [9, 'LastName', [[65, 71]]],
+            [10, 'EntityLegal', [[72, 84]]],
+            [11, 'Address', [[85, 130]]]
+          ]
+        },
+        {
           class: "reject",
-          label: "Erinne",
+          label: "Wendy",
           entities : [
             [1, 'EntityProper', [[0, 9]]],
             [2, 'EntityDescriptive', [[10, 38]]],
