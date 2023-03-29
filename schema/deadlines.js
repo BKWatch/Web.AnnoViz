b239136a8becea85cded04e2a4f01d4f26965d18
Jonathan Turkanis <j@coderage.com>
2023-03-29 21:50:58 +0000

    Added deadlines.js and code to write dimensions to log

---
+++ schema/deadlines.js
@@ -0,0 +1,136 @@
+schema = {
+    entity_types: [
+        {
+             type   : 'Enterer',
+             labels : ['Enterer', 'Ent'],
+             bgColor: '#cccccc',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'FiledBy',
+             labels : ['FiledBy', 'Filed'],
+             bgColor: '#ff6699',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Related',
+             labels : ['Related', 'Rel'],
+             bgColor: '#cceeff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Attachments',
+             labels : ['Attachments', 'Att'],
+             bgColor: '#b3e6ff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Link',
+             labels : ['Link'],
+             bgColor: '#99ddff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Error',
+             labels : ['Error', 'Err'],
+             bgColor: '#ff99cc',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Modified',
+             labels : ['Modified', 'Mod'],
+             bgColor: '#ffcc99',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Withdrawn',
+             labels : ['Withdrawn', 'Wthdrn'],
+             bgColor: '#ffff99',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Party',
+             labels : ['Party', 'Pty'],
+             bgColor: '#99ff99',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Role',
+             labels : ['Role'],
+             bgColor: '#ff9966',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Firm',
+             labels : ['Firm'],
+             bgColor: '#99ccff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'Capacity',
+             labels : ['Capacity', 'Cap'],
+             bgColor: '#ffff99',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'FeeStatus',
+             labels : ['FeeStatus', 'Fee'],
+             bgColor: '#bfbfbf',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'MeetingName',
+             labels : ['MeetingName', 'MName'],
+             bgColor: '#cc99ff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'MeetingStatus',
+             labels : ['MeetingStatus', 'MStatus'],
+             bgColor: '#cc99ff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'MeetingTime',
+             labels : ['MeetingTime', 'MTime'],
+             bgColor: '#cc99ff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'MeetingPlace',
+             labels : ['MeetingPlace', 'MPlace'],
+             bgColor: '#cc99ff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'MeetingTelecom',
+             labels : ['MeetingTelecom', 'MTele'],
+             bgColor: '#cc99ff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'DeadlineName',
+             labels : ['DeadlineName', 'DName'],
+             bgColor: '#66ccff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'DeadlineTime',
+             labels : ['DeadlineTime', 'DTime'],
+             bgColor: '#66ccff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'DeadlineParty',
+             labels : ['DeadlineParty', 'DParty'],
+             bgColor: '#66ccff',
+             borderColor: 'darken'
+        },
+        {
+             type   : 'DeadlineDocRef',
+             labels : ['DeadlineDocRef', 'DDoc'],
+             bgColor: '#66ccff',
+             borderColor: 'darken'
+        }
+    ]
+};
